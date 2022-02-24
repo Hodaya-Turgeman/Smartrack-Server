701d3d87b8dbac71c657876fa065e4e87c2dc8df
Hodaya Siman Tov <hodaysi123@gmail.com>
2022-02-24 17:40:13 +0200

    234

--- src/controller/travelerController.js
+++ src/controller/travelerController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Traveler = require('../model/traveler');
 
 const infoTraveler = (req, res) => {
-    Traveler.findOne({travelerMaill: req.params.email}).then(traveler=> {
+    Traveler.findOne({travelerMail: req.params.email}).then(traveler=> {
         res.send("True")
     }).catch(err=> {
         res.send("False")
@@ -11,7 +11,7 @@ const infoTraveler = (req, res) => {
     
 }
 const getInfoTraveler = (req, res) => {
-    Traveler.findOne({travelerMaill: req.query.travelerMail}).then(traveler=> {
+    Traveler.findOne({travelerMail: req.query.travelerMail}).then(traveler=> {
         res.send(traveler)
     }).catch(err=> {
         res.send("False")
@@ -37,4 +37,22 @@ const addTraveler= (req,res)=>{
                 res.send( 'An error User Occurred!')
         })
 }
-module.exports = {infoTraveler,addTraveler,getInfoTraveler}
+const editTraveler = (req,res)=>{
+    console.log("query", req.query)
+    const traveler=new Traveler({
+        travelerMail:req.query.travelerMail,
+        travelerName:req.query.travelerName,
+        travelerBirthYear:req.query.travelerBirthYear,
+        travelerGender:req.query.travelerGender,
+        travelerFavoriteCategories:req.query.travelerFavoriteCategories
+    })
+    console.log("traveler", traveler)
+    Employer.findOneAndUpdate({email: req.params.email},traveler, {new: true })
+        .then(response=>{
+            res.send('true')
+        })
+        .catch(error=>{
+            res.send("false")
+        })
+}
+module.exports = {infoTraveler,addTraveler,getInfoTraveler,editTraveler}
